d5b9043fc42297580c55d92eaec2eec2d4905776
vidadofael <vidadofael@gmail.com>
2023-05-09 20:45:23 -0300

    formulario pronto agora fazer o btn coletar dados?

--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
@@ -40,13 +41,16 @@ const Formulario = () => {
     return (
         <section className="formulario">
             <form>
-                <h2>Preencha todos os campos para registrar seu Currículo</h2>
+                <h1>Preencha todos os campos para registrar seu Currículo</h1>
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Email" placeholder="Digite seu email" />
                 <CampoTexto label="Cidade" placeholder="Digite sua Cidade" />
                 <CampoTexto label="Estado(UF)" placeholder="Ex.: SP" />
                 <CampoTexto label="Link do Drive que leva ao seu Currículo" placeholder=" Ex.: http://www.drive.google.com/file..." />
-                <ListaSuspensa itens={VagaPretendida}/>
+                <ListaSuspensa label="Vaga Pretendida" itens={VagaPretendida}/>
+                <Botao>
+                    Cadastrar
+                </Botao>
             </form>
         </section>
     )
--- src/Componentes/ListaSuspensa/index.js
+++ src/Componentes/ListaSuspensa/index.js
@@ -4,10 +4,10 @@ const ListaSuspensa = (props) => {
     console.log(props.itens)
     
     return (
-        <div>
+        <div className='lista-suspensa'>
             <label>{props.label}</label>
             <select>
-                {props.itens.map(item => <option>{item}</option>)}
+                {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
     )
